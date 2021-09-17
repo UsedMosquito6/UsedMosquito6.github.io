e2ac7002ab728a5b43ee1ce8fac90a18e708df17
UsedMosquito6 <90922372+UsedMosquito6@users.noreply.github.com>
2021-09-17 11:00:34 -0600

    TankG-B and Movement
    Added tankG-B, started putting tank parts together, started adding movement.

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -4,9 +4,14 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let tankGT;
+let tankGB;
+let missile;
 let radius = 25;
 let x, y, a;
+let theta;
+let bx = 0;
+let by = 0;
 let cx = 0;
 let cy = 0;
 let speed = 8;
@@ -14,8 +19,9 @@ let pos = 100;
 let type;
 
 function preload() {
-  tankG = loadImage("assets/tankG-T.png");
+  tankGT = loadImage("assets/tankG-T.png");
   missile = loadImage("assets/missile.png");
+  tankGB = loadImage("assets/tankG-B.png");
 }
 
 function setup() {
@@ -29,7 +35,9 @@ function draw() {
   background(204);
   angleMode(DEGREES); // Change the mode to DEGREES
   shoot();
+  tankBottom();
   tankTop();
+  move();
   ammoSelectionSquare();
 }
 
@@ -38,11 +46,15 @@ function draw() {
 function tankTop() {
   a = atan2(mouseY - height / 2, mouseX - width / 2);
   push();
-  translate(width / 2, height / 2);
+  cx = width / 2;
+  cy = height / 2;
+  translate(cx, cy);
+  cx = 0;
+  cy = 0;
   rotate(90);
   rotate(a);
   imageMode(CENTER);
-  image(tankG, 0, 0, radius * 2, radius * 2);
+  image(tankGT, cx, cy, radius * 2, radius * 2);
   pop();
 }
 
@@ -52,19 +64,57 @@ function shoot() {
     translate(width / 2, height / 2);
     fill("grey");
     if (type === 1) {
-      circle(cx, cy, 5);
-    } else if (type === 2) {
-      image(missile, cx, cy, 25, 5);
+      circle(bx, by, 5);
+    } 
+    else if (type === 2) {
+      image(missile, bx, by, 25, 5);
     }
     rotate(a);
-    cx += cos(a) * speed;
-    cy += sin(a) * speed;
+    bx += cos(a) * speed;
+    by += sin(a) * speed;
     pop();
-  } else {
-    cx = 0;
-    cy = 0;
+  } 
+  else {
+    bx = 0;
+    by = 0;
   }
 }
+
+function move(){
+  if (keyIsDown(68)) {
+    theta += 2.5;
+  }
+  if (keyIsDown(65)) {
+    theta -= 2.5;
+  }
+  if (keyIsDown(87)) {
+    cx += cos(theta) * speed;
+    cy += sin(theta) * speed;
+  }
+  if (keyIsDown(83)) {
+    cx -= cos(theta) * speed;
+    cy -= sin(theta) * speed;
+  }
+
+}
+
+function tankBottom(){
+  push();
+  cx = width / 2;
+  cy = height / 2;
+  translate(cx, cy);
+  cx = 0;
+  cy = 0;
+  rotate(theta);
+  imageMode(CENTER);
+  image(tankGB, cx, cy, radius * 2, radius * 2);
+  pop();
+
+}
+
+
+
+
 
 function ammoSelectionSquare() {
   rectMode(CENTER);
@@ -76,7 +126,7 @@ function ammoSelectionSquare() {
   square(pos, height - 50, 50);
   beginShape();
   vertex(pos - 20, height -20);
-  vertex(pos + 20, height -20)
+  vertex(pos + 20, height -20);
   endShape();
   image(missile, 200, height - 50, 50, 10);
 }
