c71b359e06ed840c47c36ba8a75ff5db394bf474
UsedMosquito6 <90922372+UsedMosquito6@users.noreply.github.com>
2021-09-28 10:38:03 -0600

    snake

---
+++ snake/sketch.js
@@ -0,0 +1,70 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let gridSize = 8;
+let grid;
+
+function setup() {
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+  
+  grid = createRandomGrid(gridSize);
+}
+
+function draw() {
+  background(220);
+
+  displayGrid();
+}
+
+function displayGrid() {
+  let cellSize = width/gridSize;
+
+  for (let y=0; y<grid.length; y++) {
+    for (let x=0; x<grid[y].length; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+
+function createEmptyGrid(howLarge) {
+  let emptyArray = [];
+  for (let y=0; y<howLarge; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<howLarge; x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
+}
+
+function createRandomGrid(howLarge) {
+  let emptyArray = [];
+  for (let y=0; y<howLarge; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<howLarge; x++) {
+      if (random(0, 100) < 50) {
+        emptyArray[y].push(0);
+      }
+      else {
+        emptyArray[y].push(1);
+      }
+    }
+  }
+  return emptyArray;
+}
