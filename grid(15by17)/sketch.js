863781345e6142c7194356188e2149f76dcaec4e
UsedMosquito6 <90922372+UsedMosquito6@users.noreply.github.com>
2021-10-06 10:44:36 -0600

    Fixed movement

--- grid(15by17)/sketch.js
+++ grid(15by17)/sketch.js
@@ -26,11 +26,12 @@ function draw() {
   displaySnake();
   displayGrid();
   
+  
 }
 
 
 function displayGrid() {
-  let cellSize = width/gridWidth;
+  let cellSize = windowWidth/17;
 
   for (let y=0; y<grid.length; y++) {
     for (let x=0; x<grid[y].length; x++) {
@@ -83,15 +84,66 @@ function displaySnake(){
 }
 
 function keyPressed(){
+  grid[snake[snake.length-1].y][snake[snake.length-1].x] = 0;
   if (key === "w"){
-    snake[0].y += 1;
-    for (let i=0; i<snake.length; i++){
-      snake[i].y = snake[0].y;
-    
-    }
+    if (snake[0].y >0){
+      if (grid[snake[0].y -1][snake[0].x] === 0){
+        for (let i= snake.length -1; i>0; i--){
+          snake[i].y = snake[i-1].y;
+          snake[i].x = snake[i-1].x;
+        }
+        snake[0].y -= 1;
+      }
+      //else{ game over
 
+    }
+    //else { game over
+  }
+  if (key === "a"){
+    for (let i= snake.length -1; i>0; i--){
+      snake[i].y = snake[i-1].y;
+      snake[i].x = snake[i-1].x;
+    }
+    snake[0].x -= 1;
+  }
+  if (key === "s"){
+    for (let i= snake.length -1; i>0; i--){
+      snake[i].y = snake[i-1].y;
+      snake[i].x = snake[i-1].x;
+    }
+    snake[0].y += 1;
+  }
+  if (key === "d"){
+    for (let i= snake.length -1; i>0; i--){
+      snake[i].y = snake[i-1].y;
+      snake[i].x = snake[i-1].x;
+    }
+    snake[0].x += 1;
   }
 }
+
+
+
+function mousePressed(){
+  if(mouseX <= width && mouseY <= height){
+    let cellX = Math.floor(mouseX/(width/gridWidth));
+    let cellY = Math.floor(mouseY/(height/gridHeight));
+    swap(cellX, cellY);
+  }
+}
+
+
+function swap(x, y){
+  if (x>=0 && x < gridWidth && y>=0 && y < gridHeight){
+    if (grid[y][x] === 0){
+      grid [y][x] = 1;
+    }
+    else if (grid[y][x] === 1){
+      grid [y][x] = 0;
+    }
+  }
+}
+
 
 
 
